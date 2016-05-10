3d230656879bf26666b7167d60f714c220ce8d74
pierrelouis <pierrelouismahe@gmail.com>
2016-05-10 08:34:44 +0200

    [PL] - MAJ du pseudo et la photo de profil

--- app/src/main/java/com/kioube/tourapp/android/client/ui/FacebookActivity.java
+++ app/src/main/java/com/kioube/tourapp/android/client/ui/FacebookActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -25,19 +27,31 @@ public class FacebookActivity extends Activity {
     private LoginButton loginButton;
     private CallbackManager callbackManager;
     private ProfileTracker mProfileTracker;
+    private Button button;
 
     // Passage des variables
-    final String EXTRA_FIRST_NAME = "";
+    /*final String EXTRA_FIRST_NAME = "";
     final String EXTRA_LAST_NAME = "";
-    Intent intent = null;
-
+    final String EXTRA_ID = "";*/
+    Intent returnIntent = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        returnIntent = new Intent();
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.connexion_activity);
+
+        button = (Button) findViewById(R.id.button_back);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setResult(Activity.RESULT_CANCELED, returnIntent);
+                finish();
+            }
+        });
+
         info = (TextView) findViewById(R.id.info);
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("public_profile");
@@ -46,7 +60,7 @@ public class FacebookActivity extends Activity {
             @Override
             public void onSuccess(LoginResult loginResult) {
 
-                intent = new Intent(FacebookActivity.this, MainActivity.class);
+
                 if (Profile.getCurrentProfile() == null) {
                     mProfileTracker = new ProfileTracker() {
                         @Override
@@ -56,20 +70,36 @@ public class FacebookActivity extends Activity {
                             Log.v("facebook - FirstName", profile2.getFirstName());
                             Log.v("facebook - LastName", profile2.getLastName());
                             mProfileTracker.stopTracking();
-                            intent.putExtra(EXTRA_FIRST_NAME, profile2.getFirstName());
+                            /*intent.putExtra(EXTRA_FIRST_NAME, profile2.getFirstName());
                             intent.putExtra(EXTRA_LAST_NAME, profile2.getLastName());
-                            startActivity(intent);
+                            intent.putExtra(EXTRA_ID, profile2.getId());
+                            finish();
+                            startActivity(intent);*/
+
+                            returnIntent.putExtra("EXTRA_FIRST_NAME", profile2.getFirstName());
+                            returnIntent.putExtra("EXTRA_LAST_NAME",profile2.getLastName());
+                            returnIntent.putExtra("EXTRA_ID",profile2.getId());
+                            setResult(Activity.RESULT_OK,returnIntent);
+                            finish();
+
                         }
                     };
                     mProfileTracker.startTracking();
                 } else {
                     Profile profile = Profile.getCurrentProfile();
                     Log.v("facebook - FirstName", profile.getFirstName());
+                    /*intent.putExtra(EXTRA_FIRST_NAME, profile.getFirstName());
                     Log.v("facebook - LastName", profile.getLastName());
-
-                    intent.putExtra(EXTRA_FIRST_NAME, profile.getFirstName());
                     intent.putExtra(EXTRA_LAST_NAME, profile.getLastName());
-                    startActivity(intent);
+                    Log.v("facebook - id", profile.getId());
+                    intent.putExtra(EXTRA_ID, profile.getId());
+                    finish();
+                    startActivity(intent);*/
+                    returnIntent.putExtra("EXTRA_FIRST_NAME", profile.getFirstName());
+                    returnIntent.putExtra("EXTRA_LAST_NAME",profile.getLastName());
+                    returnIntent.putExtra("EXTRA_ID",profile.getId());
+                    setResult(Activity.RESULT_OK,returnIntent);
+                    finish();
                 }
 
                 /*info.setText(
@@ -100,11 +130,13 @@ public class FacebookActivity extends Activity {
             }
         });
 
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
 
 }
